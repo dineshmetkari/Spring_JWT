7ee3b8268da525559c4b564ea42cc5b5606cb0ae
Dinesh Metkari <dajee.metkari@stackroute.in>
2017-12-27 18:29:10 +0530

    Initial changes

---
+++ src/test/java/guru/nidi/jwtspring/AbstractMvcTest.java
@@ -0,0 +1,91 @@
+package guru.nidi.jwtspring;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import guru.nidi.jwtspring.domain.User;
+
+import guru.nidi.jwtspring.controller.AuthenticationRequest;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import guru.nidi.jwtspring.domain.User;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@TestPropertySource(properties = {"spring.data.mongodb.uri=mongodb://localhost:27017/mongo"})
+@Ignore
+public class AbstractMvcTest {
+    protected MockMvc mockMvc;
+    private ObjectMapper mapper = new ObjectMapper();
+    private static Set<Class> inited = new HashSet<>();
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() {
+        mockMvc = webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+    }
+
+    @Before
+    public void init() throws Exception {
+        if (!inited.contains(getClass())) {
+            doInit();
+            inited.add(getClass());
+        }
+    }
+
+    protected void doInit() throws Exception {
+    }
+
+    protected String json(Object o) throws IOException {
+        return mapper.writeValueAsString(o);
+    }
+
+    protected ResultActions login(String username, String password) throws Exception {
+        final AuthenticationRequest auth = new AuthenticationRequest();
+        auth.setUsername(username);
+        auth.setPassword(password);
+        
+      User user=new User("john","password");
+        
+        return mockMvc.perform(
+                post("/api/public/login")
+                        .content(json(auth))
+                        .contentType(MediaType.APPLICATION_JSON));
+    }
+
+    protected String extractToken(MvcResult result) throws UnsupportedEncodingException {
+        return JsonPath.read(result.getResponse().getContentAsString(), "$.token");
+    }
+
+    /*
+     * converts a Java object into JSON representation
+     */
+     
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/guru/nidi/jwtspring/AuthenticationTest.java
@@ -0,0 +1,52 @@
+package guru.nidi.jwtspring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthenticationTest extends AbstractMvcTest {
+//    @Override
+//    protected void doInit() throws Exception {
+//        registerUser("john", "password").andExpect(status().isCreated());
+//    }
+//
+//    @Test
+//    public void userRepositoryWithoutTokenIsForbidden() throws Exception {
+//        mockMvc.perform(get("/api/users")).andExpect(status().isForbidden());
+//    }
+//
+//    @Test
+//    public void userRepositoryWithTokenIsAllowed() throws Exception {
+//        final String token = extractToken(login("john", "password").andReturn());
+//        mockMvc.perform(get("/api/users").header("Authorization", "Bearer " + token))
+//                .andExpect(status().isOk());
+//    }
+
+    @Test
+    public void loginOk() throws Exception {
+        login("john", "password")
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").exists())
+                .andExpect(jsonPath("$.user.username", equalTo("john")))
+                .andExpect(jsonPath("$.user.password").doesNotExist())
+                .andReturn();
+    }
+
+//    @Test
+//    public void loginNok() throws Exception {
+//        login("john", "wrong").andExpect(status().isForbidden());
+//    }
+
+//
+//    private ResultActions registerUser(String username, String password) throws Exception {
+//        return mockMvc.perform(
+//                post("/api/users")
+//                        .content("{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}"));
+//    }
+}
